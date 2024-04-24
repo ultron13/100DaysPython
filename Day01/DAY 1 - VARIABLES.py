76f6fd820ad75649c85305f72621ad2e61b5eccd
Thabelo Molokana <tmolokana@gmail.com>
2024-04-24 14:10:50 +0200

    new excerices done

---
+++ Day01/DAY 1 - VARIABLES.py
@@ -0,0 +1,15 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+
+c = b
+b = a
+a = c
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
