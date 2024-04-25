bf3d0ee39c71b4bdb3c1ac843aa0832059eb1ba2
Thabelo Molokana <tmolokana@gmail.com>
2024-04-25 08:40:25 +0200

    Day02

---
+++ Day02/DAY 2 - DATA TYPES.py
@@ -0,0 +1,7 @@
+two_digit_number = input("Enter a two digit number")
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+num_one = int(two_digit_number[0])
+num_two = int(two_digit_number[1])
+print(num_one + num_two)
